263b0d833762291163eae826bff6ed49c4369c0b
radekaadek <radekaadek@gmail.com>
2024-11-07 16:30:59 +0100

    stuff

--- 2/moje.py
+++ 2/moje.py
@@ -3,17 +3,11 @@ import numpy as np
 
 # load raster
 
-raster_file = "/home/ard/prg/RS1/1/R_2023/N-34-111-C-c-3-3.tif"
+raster_file = "/home/ard/prg/RS1/1/IR_2023/N-34-111-C-c-3-3.tif"
 raster_r = "/home/ard/prg/RS1/1/RGB_2023/78936_1204849_N-34-111-C-c-3-3.tif"
 
 with rasterio.open(raster_file) as src:
-    # vector_file = "features.fgb"
-
     profile = src.profile
-
-    # gdf = geopandas.read_file(vector_file)
-    # feature = gdf.iloc[0]
-
     band1 = src.read(1)
 
 with rasterio.open(raster_r) as src:
@@ -22,26 +16,31 @@ with rasterio.open(raster_r) as src:
     bandb = src.read(3)
 
 #conver to floats
-band1 = band1.astype(np.float64).copy()
-bandr = bandr.astype(np.float64).copy()
-bandg = bandg.astype(np.float64).copy()
-bandb = bandb.astype(np.float64).copy()
+band1 = band1.astype(np.float32).copy()
+bandr = bandr.astype(np.float32).copy()
+bandg = bandg.astype(np.float32).copy()
+bandb = bandb.astype(np.float32).copy()
 
 nvdil = np.array(band1 - bandr)
 nvdim = np.array(band1 + bandr)
-print(nvdil, nvdim)
 
-nvdim[(nvdim == 0) | np.isnan(nvdim)] = 0.001
 nvdi = nvdil / nvdim
+gdvi = band1 - bandg
 
-# scale to 0-255 integers
-nvdi = np.multiply(nvdi, 255)
-nvdi = np.array(nvdi, dtype=np.uint8)
-print(profile)
+nvdi[(nvdim == 0) | np.isnan(nvdim)] = 0
 
-with rasterio.open("ok.tif", "w", **profile) as save:
-    print(nvdi)
+# scale to 0-1 floats 
+nvdi = (nvdi - nvdi.min()) / (nvdi.max() - nvdi.min())
+nvdi[(nvdim == 0) | np.isnan(nvdim)] = 0
+
+profile.update(
+    dtype=rasterio.float32
+)
+with rasterio.open("NVDI.tif", "w", **profile) as save:
     save.write(nvdi, 1)
+
+with rasterio.open("GDVI.tif", "w", **profile) as save:
+    save.write(gdvi, 1)
 
 
 # save a new raster with 4 bands
@@ -53,7 +52,7 @@ profile.update(
     driver="GTiff",
 )
 
-with rasterio.open("all.tif", "w", **profile) as save:
+with rasterio.open("RGBIR.tif", "w", **profile) as save:
     save.write(bandr, 1)
     save.write(bandg, 2)
     save.write(bandb, 3)
